e3dde74b6421d7e003e66410c3bed416d6084c34
Christian Aquino <cjfaquino@gmail.com>
2022-12-15 19:46:26 -0800

    add MyFooter

---
+++ src/components/MyFooter/MyFooter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MyFooter.css';
+import Logo from './GitHub-Mark-Light-64px.png';
+
+const MyFooter = () => (
+  <footer>
+    Copyright © 2022 <a href='https://github.com/cjfaquino/'>cjfaquino</a>
+    <img src={Logo} alt='github user: cjfaquino' />
+  </footer>
+);
+
+export default MyFooter;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouterSwitch from './RouterSwitch';
+import MyFooter from './components/MyFooter/MyFooter';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterSwitch />
+    <MyFooter />
   </React.StrictMode>
 );
