b13a3f1b75fc7b8b2ac1eb1a235008372425d127
Christian Aquino <cjfaquino@gmail.com>
2022-12-15 19:37:51 -0800

    add Shopping page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './App.css';
 
-const App = () => <div className='app' />;
+const App = () => (
+  <div className='app'>
+    <h1>Home</h1>
+    <Link to='/shopping'>Shopping</Link>
+  </div>
+);
 
 export default App;
---
+++ src/components/Shopping.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Shopping = () => (
+  <div>
+    <h1>Shopping</h1>
+  </div>
+);
+
+export default Shopping;
