853891a3598ee97a9001f0c5dd90ba502a37b4f6
Christian Aquino <cjfaquino@gmail.com>
2022-12-15 20:03:49 -0800

    add Cart

--- src/RouterSwitch.jsx
+++ src/RouterSwitch.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Shopping from './components/Shopping';
+import Cart from './components/Cart';
 
 const RouterSwitch = () => (
   <Router>
     <Routes>
       <Route path='/' element={<App />} />
       <Route path='/shopping' element={<Shopping />} />
+      <Route path='/cart' element={<Cart />} />
     </Routes>
   </Router>
 );
---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MyNav from './MyNav';
+
+const Cart = () => (
+  <div>
+    <MyNav />
+    <h1>Cart</h1>
+  </div>
+);
+
+export default Cart;
