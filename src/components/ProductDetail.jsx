162c21cb3564eefd77a32ad3c0657d9a5a56dd24
Christian Aquino <cjfaquino@gmail.com>
2022-12-15 23:26:47 -0800

    handle input changes

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -4,6 +4,7 @@ import MyNav from './MyNav';
 
 const ProductDetail = () => {
   const [item, setItem] = useState({});
+  const [quantity, setQuantity] = useState(1);
   const match = useParams();
 
   const fetchItem = async () => {
@@ -17,6 +18,16 @@ const ProductDetail = () => {
 
     return () => {};
   }, []);
+
+  const handleChange = (e) => setQuantity(e.target.value);
+
+  const incQ = () => setQuantity((x) => Number(x) + 1);
+
+  const decQ = () =>
+    setQuantity((x) => {
+      if (x > 1) return x - 1;
+      return x;
+    });
 
   return (
     <div>
@@ -43,10 +54,20 @@ const ProductDetail = () => {
 
             <label htmlFor='quantity'>
               Quantity
-              <input id='quantity' type='number' placeholder='1' />
+              <input
+                id='quantity'
+                type='number'
+                value={quantity}
+                min='1'
+                onChange={handleChange}
+              />
             </label>
-            <button type='button'>-</button>
-            <button type='button'>+</button>
+            <button type='button' onClick={decQ}>
+              -
+            </button>
+            <button type='button' onClick={incQ}>
+              +
+            </button>
             <button type='button'>Add to cart</button>
           </div>
         </div>
