7335240d6659e20bf7ce10d5861d0f0dc4fe322d
Christian Aquino <cjfaquino@gmail.com>
2022-12-18 21:10:27 -0800

    style home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import saleIMG from '../assets/sale.jpg';
 
 const Home = () => (
-  <>
-    <h1>Home</h1>
-    <Link to='/shop'>Start browsing</Link>
-  </>
+  <div className='home'>
+    <div className='box '>
+      <div className='home-info'>
+        <div>
+          <h2>A Store of The Year</h2>
+          <div className='home-quote'>
+            The products in this store are being sold!
+          </div>
+          <Link to='/shop'>
+            <button type='button' className='btn-start'>
+              Start browsing
+            </button>
+          </Link>
+        </div>
+        <div>
+          <img src={saleIMG} alt='sale' className='home-image' />
+        </div>
+      </div>
+    </div>
+    <div className='line' />
+  </div>
 );
 
 export default Home;
